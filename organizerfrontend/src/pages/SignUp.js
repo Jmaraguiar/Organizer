d74d58d9bd4505775c509b0ca5a70b24007daa99
Jmaraguiar <90418883+Jmaraguiar@users.noreply.github.com>
2022-12-14 19:55:16 -0300

    Arquivo style da pagina SignUp

--- organizerfrontend/src/pages/SignUp.js
+++ organizerfrontend/src/pages/SignUp.js
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom"
 import { goToMainPage } from "../router/cordinator"
+import { SignupContainer } from "./styles/SignUpStyle"
 
 export const SignUp = (props)=>{
     const nav = useNavigate()
 
     return (
-        <div>
+        <SignupContainer>
             <h1>Sign Up</h1>
             <button onClick={()=>goToMainPage(nav)}> SignUp </button>
-        </div>
+        </SignupContainer>
+        
     )
     }
---
+++ organizerfrontend/src/pages/styles/SignUpStyle.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+
+export const SignupContainer = styled.div`
+
+`
+
+export const SignupHeader = styled.header`
+
+`
+
+export const SignupSection = styled.section`
+
+`
+
+export const SignupFooter = styled.footer`
+
+`
